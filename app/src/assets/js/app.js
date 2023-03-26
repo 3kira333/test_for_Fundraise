2f4680403c78ec5790f1449f9a948b5203e0117d
kiravin <3kira333@gmail.com>
2023-03-26 20:03:06 +0300

    css added

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -1,1 +1,14 @@
 // first commit
+const dd = document.querySelector('.dropdown-wrapper');
+const links = document.querySelectorAll('.dropdown-wrapper__list a');
+const span = document.querySelector('.dropdown-wrapper__header');
+
+dd.addEventListener('click', function() {
+  this.classList.toggle('is-active');
+});
+
+links.forEach((element) => {
+  element.addEventListener('click', function(evt) {
+    span.innerHTML = evt.currentTarget.textContent;
+  })
+})
